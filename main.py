95e771c889458d842b569be3f7b4a6e7b21a5d70
WCJerkey <professor0913@yahoo.com>
2024-03-14 00:03:07 -0400

    Made Snail into Rectangle

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ pygame.display.set_caption("Snake RPG")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
 
+
 sky_surface = pygame.image.load('./graphics/Sky.png').convert()
 ground_surface = pygame.image.load("./graphics/ground.png").convert()
 text_surface = test_font.render("My Game", False, "Black").convert()
+
+
 snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
+snail_rec = snail_surface.get_rect(midbottom = (600,300))
+
 player_surface = pygame.image.load("./graphics/Player/player_stand.png").convert_alpha()
 player_rec = player_surface.get_rect(midbottom = (80, 300))
 
-snail_x_pos = 600
+
 
 while True:
 
@@ -26,14 +31,14 @@ while True:
             exit()
 
     
-    snail_x_pos -= 4
-    player_rec.right -= 2
-    if snail_x_pos < -100: snail_x_pos = 800
+    snail_rec.right -= 4
+    player_rec.right += 1
+    if snail_rec.right < -50: snail_rec.right = 800
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    screen.blit(snail_surface, snail_rec)
     screen.blit(player_surface, player_rec)
 
 
